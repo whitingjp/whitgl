9c4a2a7a29844da992f8ab3efe301912427f9d8d
Jonathan Whiting <jonathan@ignika.com>
2014-08-10 09:59:43 +0100

    Move a bunch of build.py functionality into functions that could be used by external project

--- build.py
+++ build.py
@@ -8,65 +8,45 @@ import ninja_syntax
 plat = platform.system()
 bit64 = platform.architecture()[0] == '64bit'
 joinp = os.path.join
-
-BUILD_FILENAME = 'build.ninja'
-buildfile = open(BUILD_FILENAME, 'w')
-n = ninja_syntax.Writer(buildfile)
-
-objext = '.o'
-
-target = 'whitgl.a'
-srcdir = 'src'
-examplesrcdir = 'example'
-inputdir = 'input'
-builddir = 'build'
-exampledir = joinp(builddir, 'example')
-objdir = joinp(builddir, 'obj')
-libdir = joinp(builddir, 'lib')
-data_in =  joinp(examplesrcdir, 'data')
-data_out = joinp(exampledir, 'data')
-targets = []
-
-cflags = ''
-cflags += '-Iinc'
-cflags += ' -Wall -Wextra -Werror'
-cflags += ' -g'
-
 if(bit64):
   fmodlib = 'fmodex64'
 else:
   fmodlib = 'fmodex'
+fmodso = 'lib%s.so' % fmodlib
 
-ldflags = ''
-if(plat == 'Windows'):
-  cflags += ' -Iinput/glfw/include -Iinput/soil/include -Iinput/glew/include -Iinput/fmod/win/inc '
-  ldflags += ' -Linput/glfw/lib-mingw -Linput/glew/lib -Linput/soil -Linput/fmod/win/lib  -lSOIL input/glfw/lib-mingw/glfw3dll.a -lglu32 -lopengl32 -lglew32 -lfmodex -lpng '
-else:
-  cflags += ' -isystem input/fmod/api/inc -Iinput/glfw/include '
-  ldflags += ' -Wl,-rpath=.,--enable-new-dtags -Linput/fmod/api/lib -Linput/glfw/build/src -lglfw3 -lGLU -lGL -lGLEW -lSOIL -lm -l%s -lX11 -lXxf86vm -lpthread -lXrandr -lXi -lpng' % fmodlib
+def rules(n):
+  cflags = ''
+  cflags += '-Iinc'
+  cflags += ' -Wall -Wextra -Werror'
+  cflags += ' -g'
 
-# clfags += -pg
-# ldflags += -pg
+  ldflags = ''
+  if(plat == 'Windows'):
+    cflags += ' -Iinput/glfw/include -Iinput/soil/include -Iinput/glew/include -Iinput/fmod/win/inc '
+    ldflags += ' -Linput/glfw/lib-mingw -Linput/glew/lib -Linput/soil -Linput/fmod/win/lib  -lSOIL input/glfw/lib-mingw/glfw3dll.a -lglu32 -lopengl32 -lglew32 -lfmodex -lpng '
+  else:
+    cflags += ' -isystem input/fmod/api/inc -Iinput/glfw/include '
+    ldflags += ' -Wl,-rpath=.,--enable-new-dtags -Linput/fmod/api/lib -Linput/glfw/build/src -lglfw3 -lGLU -lGL -lGLEW -lSOIL -lm -l%s -lX11 -lXxf86vm -lpthread -lXrandr -lXi -lpng' % fmodlib
 
-n.variable('cflags', cflags)
-n.variable('ldflags', ldflags)
-n.newline()
+  n.variable('cflags', cflags)
+  n.variable('ldflags', ldflags)
+  n.newline()
 
-n.rule('cxx',
-  command='gcc -MMD -MF $out.d $cflags -c $in -o $out',
-  depfile='$out.d',
-  description='CXX $out')
-n.rule('static',
-  command='ar rcs $out $in')
-n.rule('link',
-  command='gcc $in $libs $ldflags -o $out',
-  description='LINK $out')
-n.rule('cp',
-  command='cp $in $out',
-  description='COPY $in $out')
-n.newline()
+  n.rule('cxx',
+    command='gcc -MMD -MF $out.d $cflags -c $in -o $out',
+    depfile='$out.d',
+    description='CXX $out')
+  n.rule('static',
+    command='ar rcs $out $in')
+  n.rule('link',
+    command='gcc $in $libs $ldflags -o $out',
+    description='LINK $out')
+  n.rule('cp',
+    command='cp $in $out',
+    description='COPY $in $out')
+  n.newline()
 
-def walk_src(n, path):
+def walk_src(n, path, objdir):
   obj = []
   for (dirpath, dirnames, filenames) in os.walk(path):
     for f in filenames:
@@ -78,35 +58,57 @@ def walk_src(n, path):
   n.newline()
   return obj
 
-# Library
-obj = walk_src(n, srcdir)
-staticlib = n.build(joinp(libdir, target), 'static', obj) 
-targets += staticlib
-n.newline()
+def walk_data(n, data_in, data_out):
+  data = []
+  for (dirpath, dirnames, filenames) in os.walk(data_in):
+    for f in filenames:
+      s = os.path.relpath(joinp(dirpath, f), data_in)
+      data += n.build(joinp(data_out, s), 'cp', joinp(data_in, s))
+  n.newline()
+  return data
 
-# Example
-obj = walk_src(n, examplesrcdir)
-targets += n.build(joinp(exampledir, 'example'), 'link', obj+staticlib)
-n.newline()
+def main():
+  target = 'whitgl.a'
+  srcdir = 'src'
+  examplesrcdir = 'example'
+  inputdir = 'input'
+  builddir = 'build'
+  exampledir = joinp(builddir, 'example')
+  objdir = joinp(builddir, 'obj')
+  libdir = joinp(builddir, 'lib')
+  data_in =  joinp(examplesrcdir, 'data')
+  data_out = joinp(exampledir, 'data')
+  BUILD_FILENAME = 'build.ninja'
+  buildfile = open(BUILD_FILENAME, 'w')
+  n = ninja_syntax.Writer(buildfile)
+  rules(n)
+  # Library
+  obj = walk_src(n, srcdir, objdir)
+  staticlib = n.build(joinp(libdir, target), 'static', obj)
+  targets = []
+  targets += staticlib
+  n.newline()
 
-data = []
-for (dirpath, dirnames, filenames) in os.walk(data_in):
-  for f in filenames:
-    s = os.path.relpath(joinp(dirpath, f), data_in)
-    data += n.build(joinp(data_out, s), 'cp', joinp(data_in, s))
-n.newline()
+  # Example
+  obj = walk_src(n, examplesrcdir, objdir)
+  targets += n.build(joinp(exampledir, 'example'), 'link', obj+staticlib)
+  n.newline()
 
-targets += n.build('data', 'phony', data)
-n.newline()
+  data = walk_data(n, data_in, data_out)
 
-if(plat == 'Windows'):
-  targets += n.build(joinp(exampledir, 'fmodex.dll'), 'cp', joinp(inputdir, 'fmod', 'fmodex.dll'))
-  targets += n.build(joinp(exampledir, 'glew32.dll'), 'cp', joinp(inputdir, 'glew', 'lib', 'glew32.dll'))
-  targets += n.build(joinp(exampledir, 'glfw3.dll'), 'cp', joinp(inputdir, 'glfw', 'lib-mingw', 'glfw3.dll'))
-else:
-  fmodso = 'lib%s.so' % fmodlib
-  targets += n.build(joinp(exampledir, fmodso), 'cp', joinp(inputdir, 'fmod', 'api', 'lib', fmodso))
-n.newline()
+  targets += n.build('data', 'phony', data)
+  n.newline()
 
-n.build('all', 'phony', targets)
-n.default('all')
+  if(plat == 'Windows'):
+    targets += n.build(joinp(exampledir, 'fmodex.dll'), 'cp', joinp(inputdir, 'fmod', 'fmodex.dll'))
+    targets += n.build(joinp(exampledir, 'glew32.dll'), 'cp', joinp(inputdir, 'glew', 'lib', 'glew32.dll'))
+    targets += n.build(joinp(exampledir, 'glfw3.dll'), 'cp', joinp(inputdir, 'glfw', 'lib-mingw', 'glfw3.dll'))
+  else:
+    targets += n.build(joinp(exampledir, fmodso), 'cp', joinp(inputdir, 'fmod', 'api', 'lib', fmodso))
+  n.newline()
+
+  n.build('all', 'phony', targets)
+  n.default('all')
+
+if __name__ == '__main__':
+  main()
