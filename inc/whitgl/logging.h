6f87a1c2b7a8cdb38bc5b0e3f35a72f789b5c84e
Jonathan Whiting <jonathan@ignika.com>
2015-01-03 23:27:50 +0000

    Add panic

--- inc/whitgl/logging.h
+++ inc/whitgl/logging.h
@@ -2,6 +2,8 @@
 #define WHITGL_LOGGING_H_
 
 void whitgl_logit(const char *file, const int line, const char *str, ...);
+void whitgl_panic(const char *file, const int line, const char *str, ...);
 #define WHITGL_LOG(...) whitgl_logit(__FILE__, __LINE__, __VA_ARGS__)
+#define WHITGL_PANIC(...) whitgl_panic(__FILE__, __LINE__, __VA_ARGS__)
 
 #endif // WHITGL_LOGGING_H_
--- src/whitgl/logging.c
+++ src/whitgl/logging.c
@@ -8,8 +8,17 @@ char _buffer[LOG_BUFFER_MAX];
 
 void whitgl_logit(const char *file, const int line, const char *str, ...)
 {
-  va_list args;
-  va_start(args, str);
-  vsnprintf(_buffer, LOG_BUFFER_MAX, str, args);
-  printf("%24s:%03d  %s\n", file, line, _buffer);
+	va_list args;
+	va_start(args, str);
+	vsnprintf(_buffer, LOG_BUFFER_MAX, str, args);
+	printf("%24s:%03d  %s\n", file, line, _buffer);
+}
+
+void whitgl_panic(const char *file, const int line, const char *str, ...)
+{
+	va_list args;
+	va_start(args, str);
+	vsnprintf(_buffer, LOG_BUFFER_MAX, str, args);
+	printf("PANIC %24s:%03d  %s\n", file, line, _buffer);
+	printf("PANIC %f", *(double*)0); // do something impossible to crash
 }
