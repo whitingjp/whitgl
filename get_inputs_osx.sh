51131c98c12d5a8efd5a667bbafacfc2c91c62e7
Jonathan Whiting <jonathan@ignika.com>
2015-01-05 12:16:29 +0000

    Working on osx input retrieval

---
+++ get_inputs_osx.sh
@@ -0,0 +1,20 @@
+#/usr/bin/bash
+set -e
+mkdir input
+pushd input
+git clone git://github.com/martine/ninja.git
+pushd ninja
+git checkout release
+./configure.py --bootstrap
+popd
+curl -O https://github.com/martine/ninja/raw/master/misc/ninja_syntax.py
+curl -O http://www.fmod.org/download/fmodstudio/api/Mac/fmodstudioapi10509mac-installer.dmg
+hdiutil mount fmodstudioapi10509mac-installer.dmg
+cp -r /Volumes/FMOD\ Programmers\ API\ Mac/FMOD\ Programmers\ API/api/lowlevel/ fmod
+hdiutil unmount /Volumes/FMOD\ Programmers\ API\ Mac/
+git clone https://github.com/glfw/glfw.git
+mkdir glfw/build
+pushd glfw/build
+cmake ..
+make
+popd
