e4ab3cfbe1d9c128a114140a6eface0495bff49b
Jonathan Whiting <jonathan@ignika.com>
2015-09-11 12:18:19 +0100

    Implemented png saving

--- src/whitgl/sys.c
+++ src/whitgl/sys.c
@@ -646,7 +646,7 @@ void whitgl_sys_draw_sprite_sized(whitgl_sprite sprite, whitgl_ivec frame, whitg
 	whitgl_sys_draw_tex_iaabb(sprite.image, src, dest);
 }
 
-bool whitgl_sys_load_png(const char *name, whitgl_int *outWidth, whitgl_int *outHeight, unsigned char **outData)
+bool whitgl_sys_load_png(const char *name, whitgl_int *width, whitgl_int *height, unsigned char **data)
 {
 	png_structp png_ptr;
 	png_infop info_ptr;
@@ -733,26 +733,26 @@ bool whitgl_sys_load_png(const char *name, whitgl_int *outWidth, whitgl_int *out
 	 */
 	png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_PACKING | PNG_TRANSFORM_EXPAND, NULL);
 
-	png_uint_32 width, height;
+	png_uint_32 read_width, read_height;
 	int bit_depth;
-	png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,
+	png_get_IHDR(png_ptr, info_ptr, &read_width, &read_height, &bit_depth, &color_type,
 				 &interlace_type, NULL, NULL);
-	if(outWidth)
-		*outWidth = width;
-	if(outHeight)
-		*outHeight = height;
+	if(width)
+		*width = read_width;
+	if(height)
+		*height = read_height;
 
 	unsigned int row_bytes = png_get_rowbytes(png_ptr, info_ptr);
-	if(outData)
+	if(data)
 	{
-		*outData = (unsigned char*) malloc(row_bytes * height);
+		*data = (unsigned char*) malloc(row_bytes * read_height);
 
 		png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
 
 		png_uint_32 i;
-		for (i = 0; i < height; i++)
+		for (i = 0; i < read_height; i++)
 		{
-			memcpy(*outData+(row_bytes * (i)), row_pointers[i], row_bytes);
+			memcpy(*data+(row_bytes * (i)), row_pointers[i], row_bytes);
 		}
 	}
 
@@ -765,6 +765,57 @@ bool whitgl_sys_load_png(const char *name, whitgl_int *outWidth, whitgl_int *out
 	fclose(fp);
 
 	/* That's it */
+	return true;
+}
+bool whitgl_sys_save_png(const char *name, whitgl_int width, whitgl_int height, unsigned char *data)
+{
+	FILE *fp = fopen(name, "wb");
+	if(!fp) return false;
+
+	png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (!png) return false;
+
+	png_infop info = png_create_info_struct(png);
+	if (!info) return false;
+
+	if (setjmp(png_jmpbuf(png))) return false;
+
+	png_init_io(png, fp);
+
+	png_uint_32 png_width = width;
+	png_uint_32 png_height = height;
+
+	// Output is 8bit depth, RGBA format.
+	png_set_IHDR(
+		png,
+		info,
+		png_width, png_height,
+		8,
+		PNG_COLOR_TYPE_RGBA,
+		PNG_INTERLACE_NONE,
+		PNG_COMPRESSION_TYPE_DEFAULT,
+		PNG_FILTER_TYPE_DEFAULT
+	);
+	png_write_info(png, info);
+
+	// To remove the alpha channel for PNG_COLOR_TYPE_RGB format,
+	// Use png_set_filler().
+	//png_set_filler(png, 0, PNG_FILLER_AFTER);
+
+	png_byte ** row_pointers = png_malloc (png, png_height * sizeof (png_byte *));
+	whitgl_int y;
+	for (y = 0; y < height; ++y)
+	{
+		whitgl_int size = sizeof (uint8_t) * png_width * 4;
+		// png_byte *row = png_malloc (png, sizeof (uint8_t) * png_width * 4);
+		row_pointers[y] = &data[size*y];
+	}
+
+	png_write_image(png, row_pointers);
+	png_write_end(png, NULL);
+
+	fclose(fp);
+
 	return true;
 }
 
