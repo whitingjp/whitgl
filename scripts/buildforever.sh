fabea1eead32d8489aae55bc1c2debe711a8eb0f
Jonathan Whiting <jonathan@ignika.com>
2016-01-15 12:28:21 +0000

    Adding yet more to find

--- scripts/buildforever.sh
+++ scripts/buildforever.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while sleep 1; do
-find build.py build.sh src data scripts whitgl/src whitgl/inc | entr -d -c ./build.sh "$@"
+find build.py build.sh args.txt src data scripts whitgl/src whitgl/inc | entr -d -c ./build.sh "$@"
 done
