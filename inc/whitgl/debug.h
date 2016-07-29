d8079f438caa46f7bc21219b23bdbfdc78d161b2
Jonathan Whiting <jonathan@ignika.com>
2016-07-29 12:13:56 +0100

    refining text positioning

--- inc/whitgl/debug.h
+++ inc/whitgl/debug.h
@@ -12,7 +12,7 @@ typedef struct
 {
 	whitgl_debug_menu_type type;
 	const char* name;
-	const float* float_val;
+	const whitgl_float* float_val;
 } whitgl_debug_menu_entry;
 static const whitgl_debug_menu_entry whitgl_debug_menu_entry_zero = {WHITGL_DEBUG_FLOAT, NULL, NULL};
 
@@ -24,7 +24,7 @@ typedef struct
 } whitgl_debug_menu;
 whitgl_debug_menu whitgl_debug_menu_zero();
 
-whitgl_debug_menu whitgl_debug_menu_add_float(whitgl_debug_menu debug, const char* name, const float* val);
+whitgl_debug_menu whitgl_debug_menu_add_float(whitgl_debug_menu debug, const char* name, const whitgl_float* val);
 void whitgl_debug_menu_draw(whitgl_debug_menu debug, whitgl_sprite text_sprite, whitgl_ivec draw_pos);
 
 #endif // WHITGL_DEBUG_H_
--- src/whitgl/debug.c
+++ src/whitgl/debug.c
@@ -15,7 +15,7 @@ whitgl_debug_menu whitgl_debug_menu_zero()
 	debug.num_entries = 0;
 	return debug;
 }
-whitgl_debug_menu whitgl_debug_menu_add_float(whitgl_debug_menu debug, const char* name, const float* val)
+whitgl_debug_menu whitgl_debug_menu_add_float(whitgl_debug_menu debug, const char* name, const whitgl_float* val)
 {
 	if(debug.num_entries >= WHITGL_DEBUG_MAX_ENTRIES)
 		WHITGL_PANIC("run out of debug entries");
@@ -37,13 +37,15 @@ void whitgl_debug_menu_draw(whitgl_debug_menu debug, whitgl_sprite text_sprite, 
 		{
 			case WHITGL_DEBUG_FLOAT:
 			{
-     			snprintf(buffer, sizeof(buffer), "%s %.2f", debug.entries[i].name, *debug.entries[i].float_val);
+     			snprintf(buffer, sizeof(buffer), "%5s %5.1f", debug.entries[i].name, *debug.entries[i].float_val);
 				break;
 			}
 		}
 		whitgl_iaabb backing;
 		backing.a.x = draw_pos.x-1;
-		backing.a.y = draw_pos.y-1;
+		backing.a.y = draw_pos.y;
+		if(i==0)
+			backing.a.y--;
 		backing.b.x = draw_pos.x+strlen(buffer)*text_sprite.size.x;
 		backing.b.y = draw_pos.y+text_sprite.size.y;
 		whitgl_sys_color darken = {0x00,0x00,0x00,0x60};
