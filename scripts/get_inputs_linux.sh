f9a6acd9c91d602adc897f6ee73f60afe301b62b
Jonathan Whiting <jonathan@ignika.com>
2017-02-04 12:50:20 +0000

    update get_inputs_linux script

--- scripts/get_inputs_linux.sh
+++ scripts/get_inputs_linux.sh
@@ -1,10 +1,11 @@
 #/usr/bin/bash
-sudo apt-get install ninja-build libglfw-dev libglew-dev libpng12-dev xorg-dev libglu1-mesa-dev wget libpng12-dev
+set -e
+sudo apt-get install ninja-build libglfw-dev libglew-dev libpng12-dev xorg-dev libglu1-mesa-dev wget libpng12-dev cmake
 rm -rf input
 mkdir input
 pushd input
 wget https://github.com/martine/ninja/raw/master/misc/ninja_syntax.py
-wget http://www.fmod.org/download/fmodstudio/api/Linux/fmodstudioapi10509linux.tar.gz
+read -p "Download fmodstudioapi10509linux from fmod.org, place in input folder, then press [Enter]"
 tar -xzf fmodstudioapi10509linux.tar.gz
 ln -s fmodstudioapi10509linux fmod
 git clone --depth=1 https://github.com/glfw/glfw.git
