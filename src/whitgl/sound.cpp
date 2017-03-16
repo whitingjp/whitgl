0eeeadc8696390a43c68c44ee767bb6e4594d66e
Jonathan Whiting <jonathan@ignika.com>
2017-03-16 15:25:48 +0000

    implement sfx playback

--- src/whitgl/sound.cpp
+++ src/whitgl/sound.cpp
@@ -28,12 +28,21 @@ extern "C"
 // }
 
 irrklang::ISoundEngine* irrklang_engine = NULL;
+typedef struct
+{
+	int id;
+	irrklang::ISoundSource* sound;
+} whitgl_sound;
+#define WHITGL_SOUND_MAX (64)
+whitgl_sound sounds[WHITGL_SOUND_MAX];
+int num_sounds;
 
 void whitgl_sound_init()
 {
 	irrklang_engine = irrklang::createIrrKlangDevice();
 	if(!irrklang_engine)
 		WHITGL_PANIC("Could not startup irrklang engine\n");
+	num_sounds = 0;
 }
 void whitgl_sound_shutdown()
 {
@@ -46,34 +55,41 @@ void whitgl_sound_update()
 }
 void whitgl_sound_volume(float volume)
 {
-	(void)volume;
+	irrklang_engine->setSoundVolume(volume);
 }
 void whitgl_sound_add(int id, const char* filename)
 {
-	(void)id;
-	(void)filename;
+	if(num_sounds >= WHITGL_SOUND_MAX)
+		WHITGL_PANIC("ran out of sounds");
+	sounds[num_sounds].id = id;
+
+	sounds[num_sounds].sound = irrklang_engine->addSoundSourceFromFile(filename, irrklang::ESM_NO_STREAMING, true);
+	num_sounds++;
 }
-// int _whitgl_get_index(int id)
-// {
-// 	int index = -1;
-// 	int i;
-// 	for(i=0; i<num_sounds; i++)
-// 	{
-// 		if(sounds[i].id == id)
-// 		{
-// 			index = i;
-// 			continue;
-// 		}
-// 	}
-// 	if(index == -1)
-// 		WHITGL_PANIC("ERR Cannot find sound %d", id);
-// 	return index;
-// }
+int _whitgl_get_index(int id)
+{
+	int index = -1;
+	int i;
+	for(i=0; i<num_sounds; i++)
+	{
+		if(sounds[i].id == id)
+		{
+			index = i;
+			continue;
+		}
+	}
+	if(index == -1)
+		WHITGL_PANIC("ERR Cannot find sound %d", id);
+	return index;
+}
 void whitgl_sound_play(int id, float volume, float pitch)
 {
-	(void)id;
-	(void)volume;
-	(void)pitch;
+	whitgl_int index = _whitgl_get_index(id);
+	irrklang::ISound* sound = irrklang_engine->play2D(sounds[index].sound, false, true);
+	sound->setVolume(volume);
+	sound->setPlaybackSpeed(pitch);
+	sound->setIsPaused(false);
+	sound->drop();
 }
 
 void whitgl_loop_add(int id, const char* filename)
