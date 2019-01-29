6a440784633ee6b129906c21fdbc9ff1bd67c2a9
Jonathan Whiting <jonathan@ignika.com>
2019-01-29 15:28:01 +0000

    use substantially simpler _and_ more robust alt libpng interface for reading

--- src/whitgl/sys.c
+++ src/whitgl/sys.c
@@ -1229,124 +1229,30 @@ void whitgl_sys_draw_text(whitgl_sprite sprite, const char* string, whitgl_ivec 
 
 bool whitgl_sys_load_png(const char *name, whitgl_int *width, whitgl_int *height, unsigned char **data)
 {
-	png_structp png_ptr;
-	png_infop info_ptr;
-	unsigned int sig_read = 0;
-	int color_type, interlace_type;
-	FILE *fp;
-
-	if ((fp = fopen(name, "rb")) == NULL)
-		return false;
-
-	/* Create and initialize the png_struct
-	 * with the desired error handler
-	 * functions.  If you want to use the
-	 * default stderr and longjump method,
-	 * you can supply NULL for the last
-	 * three parameters.  We also supply the
-	 * the compiler header file version, so
-	 * that we know if the application
-	 * was compiled with a compatible version
-	 * of the library.  REQUIRED
-	 */
-	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
-									 NULL, NULL, NULL);
-
-	if (png_ptr == NULL) {
-		fclose(fp);
-		return false;
-	}
-
-	/* Allocate/initialize the memory
-	 * for image information.  REQUIRED. */
-	info_ptr = png_create_info_struct(png_ptr);
-	if (info_ptr == NULL) {
-		fclose(fp);
-		png_destroy_read_struct(&png_ptr, NULL, NULL);
-		return false;
-	}
-
-	/* Set error handling if you are
-	 * using the setjmp/longjmp method
-	 * (this is the normal method of
-	 * doing things with libpng).
-	 * REQUIRED unless you  set up
-	 * your own error handlers in
-	 * the png_create_read_struct()
-	 * earlier.
-	 */
-	if (setjmp(png_jmpbuf(png_ptr))) {
-		/* Free all of the memory associated
-		 * with the png_ptr and info_ptr */
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		fclose(fp);
-		/* If we get here, we had a
-		 * problem reading the file */
-		return false;
-	}
-
-	/* Set up the output control if
-	 * you are using standard C streams */
-	png_init_io(png_ptr, fp);
-
-	/* If we have already
-	 * read some of the signature */
-	png_set_sig_bytes(png_ptr, sig_read);
-
-	/*
-	 * If you have enough memory to read
-	 * in the entire image at once, and
-	 * you need to specify only
-	 * transforms that can be controlled
-	 * with one of the PNG_TRANSFORM_*
-	 * bits (this presently excludes
-	 * dithering, filling, setting
-	 * background, and doing gamma
-	 * adjustment), then you can read the
-	 * entire image (including pixels)
-	 * into the info structure with this
-	 * call
-	 *
-	 * PNG_TRANSFORM_STRIP_16 |
-	 * PNG_TRANSFORM_PACKING  forces 8 bit
-	 * PNG_TRANSFORM_EXPAND forces to
-	 *  expand a palette into RGB
-	 */
-	png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_PACKING | PNG_TRANSFORM_EXPAND, NULL);
-
-	png_uint_32 read_width, read_height;
-	int bit_depth;
-	png_get_IHDR(png_ptr, info_ptr, &read_width, &read_height, &bit_depth, &color_type,
-				 &interlace_type, NULL, NULL);
-	if(width)
-		*width = read_width;
-	if(height)
-		*height = read_height;
-
-	unsigned int row_bytes = png_get_rowbytes(png_ptr, info_ptr);
-	if(data)
+	png_image image;
+	memset(&image, 0, (sizeof image));
+	image.version = PNG_IMAGE_VERSION;
+	if (png_image_begin_read_from_file(&image, name) != 0)
 	{
-		*data = (unsigned char*) malloc(row_bytes * read_height);
+		image.format = PNG_FORMAT_RGBA;
+		*data = (unsigned char*) malloc(PNG_IMAGE_SIZE(image));
 
-		png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
+		if(*data == NULL)
+			return false;
 
-		png_uint_32 i;
-		for (i = 0; i < read_height; i++)
+		if (png_image_finish_read(&image, NULL, *data, 0, NULL) == 0)
 		{
-			memcpy(*data+(row_bytes * (i)), row_pointers[i], row_bytes);
+			if (*data == NULL)
+				png_image_free(&image);
+			else
+				free(*data);
+			return false;
 		}
+		*width = image.width;
+		*height = image.height;
+		return true;
 	}
-
-	/* Clean up after the read,
-	 * and free any memory allocated */
-	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-
-
-	/* Close the file */
-	fclose(fp);
-
-	/* That's it */
-	return true;
+	return false;
 }
 bool whitgl_sys_save_png(const char *name, whitgl_int width, whitgl_int height, unsigned char *data)
 {
